806b7c21e9794f11b15bafb1f7985bcbdcfbfeb3
JibayMcs <JibayMcs@users.noreply.github.com>
2023-08-21 12:24:21 +0000

    Fix styling

--- src/FilamentTour.php
+++ src/FilamentTour.php
@@ -2,15 +2,14 @@
 
 namespace JibayMcs\FilamentTour;
 
-use JibayMcs\FilamentTour\Livewire\FilamentTourWidget;
 use Livewire\LivewireManager;
-use Livewire\Mechanisms\ComponentRegistry;
 
 class FilamentTour
 {
     public function openTour(string $id)
     {
-        if (app(LivewireManager::class)->current())
+        if (app(LivewireManager::class)->current()) {
             app(LivewireManager::class)->current()->dispatch('driverjs::open-tour', $id);
+        }
     }
 }
